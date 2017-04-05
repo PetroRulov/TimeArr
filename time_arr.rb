041c543363d7b2e7d1259c354189086c93d980ed
Petro Rulov <prulov.pr@gmail.com>
2017-04-05 20:23:07 +0300

    refactoredwith dividing into three methods

--- time_arr.rb
+++ time_arr.rb
@@ -2,55 +2,35 @@ require 'date'
 require 'time'
 
 class TimeArr
-
   @@quarter = 900
   @@half_hour = 1800
   @@hour = 3600
-  @@day = 86400
 
   def compose_time_list(date)
-
-    if date.include? ":"
-      date_time = Time.parse(date)
-    else
-      date_time = Time.parse(date) + @@day
-    end
-
-    i = 0
     arr = Array.new
-    arr[i] = date_time
-    i = i + 1
+    arr[0] = get_init_time(date)
+    arr = form_arr(arr, arr[arr.size - 1], arr.size, 1, @@quarter)
+    arr = form_arr(arr, arr[arr.size - 1], arr.size, 4, @@half_hour)
+    arr = form_arr(arr, arr[arr.size - 1], arr.size, 7, @@hour)
+    arr = form_arr(arr, arr[arr.size - 1], arr.size, 120, @@hour * 24)
+    return arr
+  end
 
-    time = date_time
-    loop do
-      time = time + @@quarter
-      arr[i] = time
-      i = i + 1
-      break if time - date_time == @@hour
+  def get_init_time(date)
+    if date.include? ":"
+      return Time.parse(date)
     end
+    return Time.parse(date) + @@hour * 24
+  end
 
-    half_hours = time
+  def form_arr(init_arr, time_value, counter, hours_number, lapse)
+    arr = init_arr
+    time = time_value
     loop do
-      time = time + @@half_hour
-      arr[i] = time
-      i = i + 1
-      break if time - half_hours == @@hour * 4
-    end
-
-    hours = time
-    loop do
-      time = time + @@hour
-      arr[i] = time
-      i = i + 1
-      break if time - hours == @@hour * 7
-    end
-
-    days = time
-    loop do
-      time = time + @@day
-      arr[i] = time
-      i = i + 1
-      break if time - days == @@day * 5
+      time = time + lapse
+      arr[counter] = time
+      counter += 1
+      break if time - time_value == @@hour * hours_number
     end
     return arr
   end
@@ -61,20 +41,10 @@ is_exit = "exit"
 time_arr = TimeArr.new
 loop do
   print "Input the date, please: "
-  date = gets.chomp! # examples: "22 Dec 2016" or "24 Jun 2013 09:30:00"
+  date = gets.chomp! # examples: "1 Jun" or "22 Dec 2016" or "2 May 17:31" or "26 Jun 2013 09:30:00"
   break if date == is_exit
   arr = date.split
   if arr[0].to_i > 0 and arr[0].to_i <= 31
-    if arr[0].to_i < 10
-      arr[0] = "0" + arr[0].to_i.to_s
-      i = 0
-      date = ""
-      loop do
-        break if i >= arr.size
-        date = date.concat(arr[i] + " ")
-        i = i + 1
-      end
-    end
     time_arr.compose_time_list(date).each{|elem|puts elem.rfc2822}
     puts
   else
